86a19d0b272323afb15c6e216e18366bd286c847
wputra <wasis.putra@gmail.com>
2014-09-12 03:10:16 +0000

    exclude

--- rsync.sh
+++ rsync.sh
@@ -2,7 +2,7 @@
 
 for i in 1 2 3 4 5 6 7 8 9
 do
-rsync -avz node-$i:/etc node-$i
+rsync -avz --delete --exclude-from 'exclude-etc.txt' node-$i:/etc node-$i
 #rsync -avz --max-size=100M node-$i:/root node-$i
-rsync -avz node-$i:/usr/lib/python2.6/site-packages node-$i
+rsync -avz --delete --exclude-from 'exclude-python.txt' node-$i:/usr/lib/python2.6/site-packages node-$i
 done
